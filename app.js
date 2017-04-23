7c9861913ab986a4dc089f1fb9f98009e45b6bf3
James Beardsley <beards18@gmail.com>
2017-04-23 01:10:43 -0400

    testing actions
    just throwing some action commands in to see if it's working. it is!

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ var express = require('express')
 var app = express()
 var http = require('http')
 
+var action = require('./controllers/actions.js');
+
 var game = require('./controllers/game.js');
 var options = {
 	"gameName": "My Game",
 	"numberPlayers": 4,
 	"playerNames": ["James", "Kelly", "Conrad", "Margot"]
 }
-var newGame = game(options);
+// var newGame = game(options);
+var newGame = require('./scratchpad/sampleGame.js')
 
 app.get('/', function(req, res){
 	var output = "<!doctype HTML><html><body>";
-	output += JSON.stringify(newGame);
+	action.placeArmies(newGame, "kinabra", "James", 3)
+	output += JSON.stringify(newGame.map["kinabra"]);
 	output += "</body></html>";
 	res.send(output)
 })
